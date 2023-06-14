0e4f2dae2db597b8a94c0c467f3726edc1b01ffb
aaronlamoreaux <lam22013@byui.edu>
2023-06-13 18:35:40 -0700

    assignment 1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
+const lesson1Controller = require('../controllers');
  
 routes.get('/', lesson1Controller.homeroute);
 routes.get('/anotherperson', lesson1Controller.anotherpersonroute);
