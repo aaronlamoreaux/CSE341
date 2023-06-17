71b3477d1339cb7e3697866e4b81bf84716eff09
aaronlamoreaux <lam22013@byui.edu>
2023-06-17 14:02:42 -0700

    assignment 2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,40 +1,36 @@
 const mongodb = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
 
-const homeroute = (req, res) => {
-    res.send("Alan Lamoreaux");
+const homeRoute = (req, res) => {
+  res.send('Alan Lamoreaux');
 };
 
-const anotherpersonroute = (req, res) => {
-    res.send("Amy Lamoreaux");
+const anotherPersonRoute = (req, res) => {
+  res.send('Amy Lamoreaux');
 };
 
 const getAll = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('contacts').find();
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+  const result = await mongodb.getDb().db().collection('contacts').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
 };
 
 const getSingle = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db()
-        .collection('contacts')
-        .find({
-            _id: userId
-        });
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('contacts').find({
+    _id: userId
+  });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
 };
 
 module.exports = {
-    homeroute,
-    anotherpersonroute,
-    getAll,
-    getSingle
+  homeRoute,
+  anotherPersonRoute,
+  getAll,
+  getSingle
 };
