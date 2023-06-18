8577ef49e5193b580b74d2b0e3e05ec748dfa01d
aaronlamoreaux <lam22013@byui.edu>
2023-06-17 21:49:01 -0700

    assignment 3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -28,9 +28,43 @@ const getSingle = async (req, res, next) => {
   });
 };
 
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  res.status(201).json(response);
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb.getDb().db().collection('contacts').replaceOne({_id: userId}, contact);
+  res.status(201).json(result);
+}
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('contacts').deleteOne({_id: userId});
+  res.status(201).json(result);
+}
+
 module.exports = {
   homeRoute,
   anotherPersonRoute,
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
