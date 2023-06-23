1d688ef9ca9097aa1af981d96e22bda9404aec2c
aaronlamoreaux <lam22013@byui.edu>
2023-06-23 13:34:54 -0700

    assignment 4

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app
     next();
   })
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  .use(express.cors())
+  .use(cors())
   .use(express.urlencoded({
     extended: true
   }))
