796a0dfcd782a12e61f0c17d82279da5e26443a9
aaronlamoreaux <lam22013@byui.edu>
2023-06-23 14:09:35 -0700

    assignment 4

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,25 +10,22 @@ const anotherPersonRoute = (req, res) => {
 };
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find().catch();
+  const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    })
-    .catch();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
 };
 
 const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').find({
-      _id: userId
-    })
-    .catch();
+    _id: userId
+  });
   result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
-    })
-    .catch();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
 };
 
 const createContact = async (req, res) => {
@@ -39,8 +36,8 @@ const createContact = async (req, res) => {
     favoriteColor: req.body.favoriteColor,
     birthday: req.body.birthday
   };
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact).catch();
-  res.status(201).json(response).catch();
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  res.status(201).json(response);
 };
 
 const updateContact = async (req, res) => {
@@ -60,17 +57,16 @@ const updateContact = async (req, res) => {
         _id: userId
       },
       contact
-    )
-    .catch();
-  res.status(204).json(result).catch();
+    );
+  res.status(204).json(result);
 };
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').deleteOne({
     _id: userId
-  }).catch();
-  res.status(200).json(result).catch();
+  });
+  res.status(200).json(result);
 };
 
 module.exports = {
