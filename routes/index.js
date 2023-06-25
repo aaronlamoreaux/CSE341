91748308760f072931184df83191c4632c770f03
aaronlamoreaux <lam22013@byui.edu>
2023-06-24 20:13:44 -0700

    assignment 6

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
+routes.use('/', require('./swagger'));
 routes.use('/user', require('./user'));
 routes.use('/quote', require('./quotes'));
 
