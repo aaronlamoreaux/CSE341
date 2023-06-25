1aafcb4f2164bf8e1f33c0e80801d7019bae5637
aaronlamoreaux <lam22013@byui.edu>
2023-06-24 20:17:25 -0700

    assignment 6

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
 app
+  .use(express.json())
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,7 +19,12 @@ app
   .use(express.urlencoded({
     extended: true
   }))
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(cors())
   .use('/', require('./routes'));
+
 
 mongodb.initDb((err) => {
   if (err) {
